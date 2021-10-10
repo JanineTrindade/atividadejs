51578d3593c76a521e09b795f8af1d0767eb6ffa
JanineTrindade <janine.tquadros@outlook.com>
2021-10-10 13:54:34 -0300

    hello world

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // Operações aritiméticas
 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res)=>{
+res.statusCode = 200;
+res.setHeader('Content.Type','text/plain');
+res.end('hello world');
+
+});
+
+server.listen(port,hostname,() => {
+    console.log("servidor rodando");
+})
+
 var valor1=10;
 var valor2= 5;
 var valor3= 15;
@@ -7,3 +23,5 @@ var total;
 
 total = (valor1 + valor3) / valor2 * valor1;
 console.log(total);
+
+
